37bf042a7984a7852f5fea2039c1c06720eebffd
Sasindu Fernando <pujana.sasindu@gmail.com>
2024-06-13 03:48:54 +0530

    create toggle button in responsive navbar

--- juris_connect/src/components/navbar.jsx
+++ juris_connect/src/components/navbar.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "../navbar.css";
 
 function Navbar() {
+    const [active, setActive] = useState("nav_menu");
+    const [toggleIcon, setToggleIcon] = useState("nav_toggler");
+
+    const navToggle = () => {
+        active === 'nav_menu' ? setActive('nav_menu nav_active') : setActive('nav_menu');
+
+        toggleIcon === "nav_toggler" ? setToggleIcon("nav_toggler toggle") : setToggleIcon("nav_toggler");
+    };
+
     return (
         <nav className="nav">
             <a href="#" className="nav_brand">JURISCONNECT</a>
-            <ul className="nav_menu">
+            <ul className={active}>
                 <li className="nav_item"><a href="#" className="nav_link">Home</a></li>
                 <li className="nav_item"><a href="#" className="nav_link">Community Forum</a></li>
                 <li className="nav_item"><a href="#" className="nav_link">About Us</a></li>
             </ul>
             <button className="login_btn">Login</button>
-            <div className="nav_toggler">
+            <div onClick = {navToggle} className={toggleIcon}>
             <div className="line1"></div>
             <div className="line2"></div>
             <div className="line3"></div>
