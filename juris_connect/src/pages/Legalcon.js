dbd5cb95d55fd327d9815288f7a2f113b2ae705b
Sasindu Fernando <pujana.sasindu@gmail.com>
2024-06-14 22:29:56 +0530

    legal consultion page button created

--- juris_connect/src/pages/Legalcon.js
+++ juris_connect/src/pages/Legalcon.js
@@ -1,12 +1,25 @@
 import Navbar from '../components/Header/navbar'
+import "./Legalcon.css"
+import img1 from "../Assets/Legalcon.jpeg"
+
 
 export default function Legalcon(){
     return(
-        <>
-        <Navbar />
-        <h2>
-            Legal page
-        </h2>
-        </>
+        <div>
+      <Navbar />
+      <div className="content">
+        <div className="left">
+          <h1>Get the legal help <br/> you need</h1>
+          <p>Every problem is unique, and we offer personalized <br />recomendations to suit your specific needs</p>
+          <div className='buttons'>
+          <button className='button-left'>Ask your legal question</button>
+          <button className='button-right'>Free legal advice</button>
+          </div>
+        </div>
+        <div className="right">
+          <img src={img1} alt="Description" />
+        </div>
+      </div>
+    </div>
     )
 }
